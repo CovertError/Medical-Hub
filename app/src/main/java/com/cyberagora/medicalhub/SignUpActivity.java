ba12465e416374df403e46735e809f47dad75bb9
HajjCare <42049245+HajjCare@users.noreply.github.com>
2018-11-18 19:53:34 +0400

    checking github

--- app/src/main/java/com/cyberagora/medicalhub/SignUpActivity.java
+++ app/src/main/java/com/cyberagora/medicalhub/SignUpActivity.java
@@ -83,6 +83,7 @@ public class SignUpActivity extends AppCompatActivity {
                     {
                         if (task.isSuccessful()) {
                             //TODO Actually recording the users in the database since its not this will be commented
+                            //checking github
                             /*dbRef.child("users").child("username").addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
