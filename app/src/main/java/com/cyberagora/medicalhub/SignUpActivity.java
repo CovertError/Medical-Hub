d8ee9d3a23c54c692518f31660d678b698ca78e8
CovertError <coverterror@gmail.com>
2018-11-18 20:17:00 +0400

    HajjCare problem

--- app/src/main/java/com/cyberagora/medicalhub/SignUpActivity.java
+++ app/src/main/java/com/cyberagora/medicalhub/SignUpActivity.java
@@ -83,7 +83,7 @@ public class SignUpActivity extends AppCompatActivity {
                     {
                         if (task.isSuccessful()) {
                             //TODO Actually recording the users in the database since its not this will be commented
-                            //github edited by fixed to CovertError maybe
+                            //Hajjcare problem
                             /*dbRef.child("users").child("username").addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
